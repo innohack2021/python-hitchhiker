7c6403f52d81978281a3eab6cd13cee5fec45667
S0YKIM <soyeonkim.9683@gmail.com>
2021-11-25 22:22:11 +0900

    Files integrated

--- back-end/database.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('test-db', 'user', 'password', {
-	dialect: 'sqlite',
-	host: './dev.sqlite', // host: ':memory'
-});
-module.exports = sequelize;
